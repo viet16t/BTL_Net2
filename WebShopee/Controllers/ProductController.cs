d53d949cc14b0f459f0760b0c945e3cffd2f7284
Việt Quốc <you@example.com>
2022-12-15 07:20:07 +0700

    BTL

---
+++ WebShopee/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebShopee.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
